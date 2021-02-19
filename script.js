2d2ee2b6ec75ad3caad0d3129e6c00968ef03f98
Zahra <hizahrazadeh@gmail.com>
2021-02-19 10:23:50 -0500

    creating a new user

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
-fetch('https://reqres.in/api/users/23')
-    .then(res => {
-    if (res.ok) {
-        console.log('Success')
-    } else {
-        console.log('Not Successful')
-    }
+fetch('https://reqres.in/api/users', {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+        name: 'User 1'
+    })
+}).then(res => {
+        return res.json()
     })
     .then(data => console.log(data))
+    .catch(error => console.log('ERROR'))
 
